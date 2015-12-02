fec7667ca3f431940600378d9d8a31a1f80f41cc
yindonghai <1434577136@qq.com>
2015-12-02 18:35:11 +0800

    主流程细节完善，产品相册功能修复

--- applications/b2c/controller/admin/goods/editor.php
+++ applications/b2c/controller/admin/goods/editor.php
@@ -36,7 +36,7 @@ class b2c_ctl_admin_goods_editor extends desktop_controller
     }
 
     public function save()
-    {print_r($_POST);exit;
+    {
         $this->begin($_POST['redirect']);
         $mdl_goods = $this->app->model('goods');
         $mdl_products = $this->app->model('products');
