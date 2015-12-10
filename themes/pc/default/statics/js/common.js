869305684248513c885c9cea6cc59656b00cf4f0
yindonghai <1434577136@qq.com>
2015-12-10 19:09:11 +0800

    快捷登录弹层  2015-12-10

--- themes/pc/default/statics/js/common.js
+++ themes/pc/default/statics/js/common.js
@@ -6,12 +6,17 @@ $(function(){
      */
     var siteNav=$('.site_nav')[0];
     var webMenu=$('.website_menu')[0];
+    var timer=null;
     webMenu.onmouseover=siteNav.onmouseover=function(){
+        clearInterval(timer);
         webMenu.style.display="block";
         $('.site_nav a i').attr('class','icon-angle-up');
     }
     webMenu.onmouseout=siteNav.onmouseout=function(){
-        webMenu.style.display="none";
+        timer=setTimeout(hide,100);
+        function hide(){
+            webMenu.style.display="none";
+        }
         $('.site_nav a i').attr('class','icon-angle-down');
     }
     
@@ -119,9 +124,14 @@ $(function(){
     $('.filebox input[type="file"]').fileupload({
 
       add: function(e, data) {
+          
+          /*console.log(!data.files[0]['name'].match(/.jpg/))
+          if (!data.files[0]['name'].match(/.jpg/)) {
+              alert('非法上传，不是图片类型');
+              return false;
+          }*/
           if (!data.files[0]['type'].match(/^image/)) {
               alert('非法上传，不是图片类型');
-              //e.stopPropagation();
               return false;
           }
           data.submit();
@@ -132,6 +142,7 @@ $(function(){
           $(inputfile).siblings('.showImg').find('img').addClass('hidden');
       },
       done: function(e, data) {
+
           var inputfile = e.target || e.srcElement;
           function loadinghide(){
               $(inputfile).siblings('.showImg').find('.loading').addClass('hidden');
@@ -140,7 +151,6 @@ $(function(){
           setTimeout(loadinghide,100);
           var re = $.parseJSON(data.result);
           var input = e.target || e.srcElement;
-          
           $(input).prev('input[type="hidden"]').attr('value', re.image_id);
           $(input).next('.showImg').find('img').attr('src',re.url);
       }
