4c530e6f65321e5d7c15a1a33415a47f97e96ae8
yindonghai <1434577136@qq.com>
2015-12-23 15:05:10 +0800

    收藏夹（店铺收藏）

--- themes/pc/default/statics/js/common.js
+++ themes/pc/default/statics/js/common.js
@@ -62,6 +62,8 @@ $(function() {
 
     tabslider('.fav_goods .fav_g_nav ul li', '.fav_g_con > div', 'active'); //我的收藏-商品收藏调用
 
+    tabslider('.fav_shop .fav_s_nav ul li', '.fav_s_con > div', 'active'); //我的收藏-商品收藏调用
+
     tabslider('.like_menu span a', '.like_con > ul', 'active'); //购物车浏览记录
 
 
