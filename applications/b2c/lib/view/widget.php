b7429f16fdb317f06cd3aca628cdaf703d4393c8
lizhuang258 <lizhuang258@126.com>
2016-01-19 16:33:22 +0800

    首页楼层标题和商品挂件修改

--- applications/b2c/lib/view/widget.php
+++ applications/b2c/lib/view/widget.php
@@ -69,9 +69,10 @@ class b2c_view_widget {
 
     //楼层左侧推荐
     public function function_WIDGET_B2C_INDEX_LEFT_GOOD($params, &$smaryt) {
-        $render = new base_render(app::get('b2c'));
+        $render = new base_render(app::get($params['app']));
+        $render->pagedata['position_id'] = $params['position_id'];
         //$render->pagedata['floor_left'] = vmc::service('view_datasetting')->floor_left($params);
-        $render->pagedata['contents']= app::get('b2c')->model('pages_content')->getList('*', array('type' => '2', 'status' => '1','position_id'=>'4',));
+        $render->pagedata['contents'] = app::get('b2c')->model('pages_content')->getList('*', array('type' => '2', 'status' => '1','position_id'=>'4',));
         return $render->fetch('widget/index_left_good.html');
     }
 
@@ -85,9 +86,9 @@ class b2c_view_widget {
     // 首页-楼层
     public function function_WIDGET_B2C_GOODS_INDEX_GOOD_FLOOR($params, &$smarty) {
         $render = new base_render(app::get('b2c'));
-       // $render->pagedata['window'] = $params['window'];
+        $render->pagedata['num'] = $params['num'];
         //$render->pagedata['goods'] = vmc::service('view_datasetting')->floor($params);
-         $render->pagedata['contents']= app::get('b2c')->model('pages_content')->getList('*', array('type' => '2', 'status' => '1','position_id'=>'4',));
+         $render->pagedata['contents']= app::get('b2c')->model('pages_content')->getList('*', array('type' => '2',));
         return $render->fetch('widget/good.floor.html');
     }
 
@@ -194,6 +195,7 @@ class b2c_view_widget {
     public function function_WIDGET_B2C_GOODS_INDEX_GOOD_FLOOR_TEST($params, &$smarty) {
         $render = new base_render(app::get($params['app']));
         //$render->pagedata['floor'] = vmc::singleton('b2c_source_goods')->floor($params);
+        $render->pagedata['floor_title']= app::get('b2c')->model('pages_position')->getList('*', array('type' => '2', 'status' => '1',));
         $render->pagedata['contents']= app::get('b2c')->model('pages_content')->getList('*', array('type' => '2', 'status' => '1',));
         return $render->fetch('widget/floor.test.html');
     }
