13622aac62629ebe6392f438937ed1a1a30d6292
libing <1691285988@qq.com>
2016-03-17 17:06:48 +0800

    [bug fix] 重建接口app

---
+++ applications/apicenter/lib/interaction/product.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | VMCSHOP [V M-Commerce Shop]
+// +----------------------------------------------------------------------
+// | Copyright (c) vmcshop.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.vmcshop.com/licensed)
+// +----------------------------------------------------------------------
+// | Author: Shanghai ChenShang Software Technology Co., Ltd.
+// +----------------------------------------------------------------------
+
+/**
+ * 货品相关
+ */
+class apicenter_interaction_product
+{
+    private $URL;
+    private $request;
+    public function __construct()
+    {
+        header("Content-type:text/html;charset=utf-8");
+        $this->URL = 'msk-web/api/v1/ms/';
+        $this->request = vmc::singleton('apicenter_api');
+    }
+
+}
