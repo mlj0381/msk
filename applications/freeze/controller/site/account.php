aadda3dd82bd1160fd80be1af999c57bf8bd7071
chen peng <476451758@qq.com>
2016-03-20 09:48:51 +0800

    最新更新

--- applications/freeze/controller/site/account.php
+++ applications/freeze/controller/site/account.php
@@ -4,6 +4,7 @@ class freeze_ctl_site_account extends freeze_frontpage
     function __construct(&$app)
     {
         parent::__construct($app);
+        $this->verify_member();
         $this->menuSetting = 'account';
     }
 
@@ -53,6 +54,8 @@ class freeze_ctl_site_account extends freeze_frontpage
      */
     public function buyer_info()
     {
+        $this->is_complete_info();
+
         $this->output();
     }
 
@@ -61,9 +64,109 @@ class freeze_ctl_site_account extends freeze_frontpage
      */
     public function security()
     {
+        $this->is_complete_info();
+
+        $user_obj = vmc::singleton('freeze_user_object');
+        $this->pagedata['pam_data'] = $user_obj->get_pam_data('*',$user_obj->get_member_id());
         $this->output();
     }
 
+    /**
+     * 设置手机号码
+     */
+    public function set_pam_mobile($action)
+    {
+        if($action == 'save')
+        {
+            $redirect_here = array('app' => 'freeze', 'ctl' => 'site_account', 'act' => 'set_pam_mobile');
+            $redirect = $this->gen_url(array('app' => 'freeze', 'ctl' => 'site_account', 'act' => 'security'));
+            $params = $_POST;
+            $mobile = $params['mobile'];
+            if (!vmc::singleton('b2c_user_vcode')->verify($params['vcode'], $params['mobile'], 'freeze_reset')) {
+                $this->splash('error', $redirect_here, '手机短信验证码不正确');
+            }
+
+            if (!vmc::singleton('freeze_user_passport')->set_mobile($mobile, $msg)) {
+                $this->splash('error', $redirect_here, $msg);
+            } else {
+                $this->splash('success', $redirect, $msg);
+            }
+        }else{
+            $user_obj = vmc::singleton('freeze_user_object');
+            $info = $user_obj->get_members_data(array('freeze'=>'name'));
+            if($mobile = $user_obj->_get_pam_type_data('login_account','mobile'))
+            {
+                $info['freeze']['mobile'] = $mobile['login_account'];
+            };
+            $this->pagedata['freeze'] = $info['freeze'];
+            $this->output();
+        }
+    }
+
+    /**
+     * 修改登录密码
+     */
+    public function reset_password($action)
+    {
+        $this->title = '重置密码';
+        if ($action == 'doreset') {
+            $passport_obj = vmc::singleton('freeze_user_passport');
+            $redirect_here = array('app' => 'freeze', 'ctl' => 'site_account', 'act' => 'reset_password');
+            $params = $_POST;
+            $forward = $params['forward'];
+            if (!$forward) {
+                $forward = $this->gen_url(array(
+                    'app' => 'freeze',
+                    'ctl' => 'site_account',
+                    'act' => 'security'
+                ));
+            }
+            if (empty($params['new_password'])) {
+                $this->splash('error', $redirect_here, '请输入新密码!');
+            }
+            if ($params['new_password1'] != $params['new_password']) {
+                $this->splash('error', $redirect_here, '两次输入的密码不一致!');
+            }
+
+            if (!vmc::singleton('b2c_user_vcode')->verify($params['vcode'], $params['mobile'], 'freeze_reset')) {
+                $this->splash('error', $redirect_here, '手机短信验证码不正确');
+            }
+
+            $result = app::get('pam')->model('freeze')->getRow('freeze_id', array('login_account' => $params['mobile'],'login_type'=>'mobile'));
+            if(empty($result)){
+                $this->splash('error', $redirect_here, '未知帐号!');
+            }
+            $member_id = $result['freeze_id'];
+            if (!$passport_obj->reset_password($member_id, $params['new_password'])) {
+                $this->splash('error', $redirect_here, '密码重置失败!');
+            }
+
+            //退出当前登录状态
+            $auth = pam_auth::instance(pam_account::get_account_type($this->app->app_id));
+            if(!$auth->type)
+            {
+                $auth->type = $this->app->app_id;
+            }
+            foreach (vmc::servicelist('passport') as $k => $passport) {
+                $passport->loginout($auth);
+            }
+
+            $redirect = $this->gen_url(array('app' => 'freeze', 'ctl' => 'site_passport', 'act' => 'login'));
+            $this->splash('success', $redirect, '密码重置成功，请重新登录');
+        } else {
+            $user_obj = vmc::singleton('freeze_user_object');
+            $mobile = $user_obj->_get_pam_type_data('login_account','mobile');
+
+            $this->pagedata['mobile'] = $mobile['login_account'];
+           $this->output();
+        }
+    }
+
+
+
+    /**
+     * 管家信息管理数据组织
+     */
     public function generate(&$data,&$msg)
     {
         $user_obj = vmc::singleton('freeze_user_object');
@@ -77,6 +180,8 @@ class freeze_ctl_site_account extends freeze_frontpage
         $generate_data['ID'] = $data['ID'];
         $generate_data['area'] = $data['area'];
         $generate_data['address'] = $data['address'];
+        $generate_data['manage_area'] = $data['manage_area'];
+        $generate_data['manage_address'] = $data['manage_address'];
         $generate_data['QQ'] = $data['QQ'];
         $generate_data['WeChat'] = $data['WeChat'];
         $generate_data['head_image'] = $data['head_image'];
@@ -100,13 +205,63 @@ class freeze_ctl_site_account extends freeze_frontpage
             $msg  = '无效基本信息';
             return false;
         }
-        if($freeze_model->count(array('ID'=>$data['ID'],'freeze_id|notin'=>$generate_data['freeze_id'])) >0)
-        {
-            $msg  = '身份证已存在';
-            return false;
-        };
+//        if($freeze_model->count(array('ID'=>$data['ID'],'freeze_id|notin'=>$generate_data['freeze_id'])) >0)
+//        {
+//            $msg  = '身份证已存在';
+//            return false;
+//        };
         $data  = $generate_data;
         return true;
+    }
+
+    //发送短信验证码
+    public function send_vcode($type = 'freeze_reset')
+    {
+        $this->passport_obj = vmc::singleton('freeze_user_passport');
+        $mobile = trim($_POST['mobile']);
+        if ($_POST['type'] != 'securitycenter') {
+            /*
+             * 不用检查手机号是否存在
+            if (!$this->passport_obj->check_signup_account($mobile, $msg)) {
+                $this->splash('error', null, $msg);
+            }
+            */
+            $msg = $this->passport_obj->get_login_account_type($mobile);
+            if ($msg != 'mobile') {
+                $this->splash('error', null, '错误的手机格式');
+            }
+        } else {
+            if ('mobile' != $this->passport_obj->get_login_account_type($_POST['mobile'])) {
+                $this->splash('error', null, '错误的手机格式');
+            }
+        }
+
+        $uvcode_obj = vmc::singleton('b2c_user_vcode');
+        $vcode = $uvcode_obj->set_vcode($mobile, $type, $msg);
+        $this->splash('success', $vcode, '短信已发送'); // 2015/9/7 短信直接显示
+
+        if ($vcode) {
+            //发送验证码 发送短信
+            $data['vcode'] = $vcode;
+            if (!$uvcode_obj->send_sms($type, (string)$mobile, $data)) {
+                $this->splash('error', null, '短信发送失败');
+            }
+        } else {
+            $this->splash('failed', null, $msg);
+        }
+        $this->splash('success', null, '短信已发送');
+    }
+
+    public function set_forward(&$forward)
+    {
+        $params = $this->_request->get_params(true);
+        $forward = ($forward ? $forward : $params['forward']);
+        if (!$forward) {
+            $forward = $_SERVER['HTTP_REFERER'];
+        }
+        if (!strpos($forward, 'passport')) {
+            $this->pagedata['forward'] = $forward;
+        }
     }
 
 }
