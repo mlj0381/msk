1efa7d05f3dc88fef52dff65737572c7437fb891
chen peng <476451758@qq.com>
2016-03-21 11:07:46 +0800

    添加买家冻品管家页面

--- applications/freeze/controller/site/list.php
+++ applications/freeze/controller/site/list.php
@@ -15,6 +15,15 @@ class freeze_ctl_site_list extends freeze_frontpage
         $this->page('site/list/freeze_list.html');
     }
 
+    /**
+     * 管家商品列表
+     */
+    public function freeze_goods()
+    {
+        $this->set_tmpl('default');
+        $this->page('site/list/freeze_goods.html');
+    }
+
 }
 
 ?>
