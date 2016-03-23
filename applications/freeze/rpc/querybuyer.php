105ad31525139910b647672889a1a80508d3b4e6
chengzhi <1847265285@qq.com>
2016-03-23 18:45:44 +0800

    冻品管家接口信息

--- applications/freeze/rpc/querybuyer.php
+++ applications/freeze/rpc/querybuyer.php
@@ -59,63 +59,46 @@ $remote['freeze_inquirebuyer'] = array(
             'name' => '1:专属买家、2：抢单买家',
             'coloumn' => 'buyerFlag',
             'type' => 'String',
-            'default' => '',
-            'require' => ''
         ),
         'slCode' => array(
             'name' => '买手店编码',
             'coloumn' => 'slCode',
             'type' => 'String',
-            'default' => '',
-            'require' => ''
         ),
         'houseAccount' => array(
             'name' => '管家账号',
             'coloumn' => 'houseAccount',
             'type' => 'String',
-            'default' => '',
-            'require' => ''
         ),
         'buyerid' => array(
             'name' => '买家编码',
             'coloumn' => 'buyerid',
             'type' => 'String',
-            'default' => '',
-            'require' => ''
         ),
         'startTime' => array(
             'name' => '开始日时',
             'coloumn' => 'startTime',
             'type' => 'datetime',
-            'default' => '',
-            'require' => ''
         ),
         'applyTime' => array(
             'name' => '申请日时',
             'coloumn' => 'applyTime',
             'type' => 'datetime',
-            'default' => '',
-            'require' => ''
         ),
         'applyStatus' => array(
             'name' => '申请状态',
             'coloumn' => 'applyStatus',
             'type' => 'String',
-            'default' => '1：申请中、2：专属会员',
-            'require' => ''
         ),
         'applySide' => array(
             'name' => '认证方式',
             'coloumn' => 'applySide',
             'type' => 'String',
-            'default' => 'A：冻品管家发展专属会员买家 B：买家选择专属冻品管家',
-            'require' => ''
         ),
         'ver' => array(
             'name' => '版本号',
             'coloumn' => 'ver',
             'type' => 'Integer',
-            'default' => '', 'require' => ''
         )
     ),
 );
