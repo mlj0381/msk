cbd2ef5fdfba1ab270932609936eecd0bc723105
chengzhi <1847265285@qq.com>
2016-03-25 12:03:07 +0800

    查询买家基本信息接口信息

--- applications/b2c/rpc/select_member_base_info.php
+++ applications/b2c/rpc/select_member_base_info.php
@@ -26,7 +26,7 @@ $remote['b2c_select_member_base_info'] = array(
             'name' => '买家ID',
             'column' => 'buyerId',
             'type' => 'String',
-            'require' => false
+            'require' => true,
         ),
     ),
 
@@ -36,14 +36,89 @@ $remote['b2c_select_member_base_info'] = array(
             'column' => 'buyerId',
             'type' => 'String',
         ),
-        'recPerType' => array(
-            'name' => '收货时间段类型',
-            'column' => 'recPerType',
+        'buyerCode' => array(
+            'name' => '买家编码',
+            'column' => 'buyerCode',
             'type' => 'String',
         ),
-        'timeDescribe' => array(
-            'name' => '时间描述',
-            'column' => 'timeDescribe',
+        'buyerName' => array(
+            'name' => '买家名称',
+            'column' => 'buyerName',
+            'type' => 'String',
+        ),
+        'buyerAddr' => array(
+            'name' => '买家地址',
+            'column' => 'buyerAddr',
+            'type' => 'String',
+        ),
+        'superiorId' => array(
+            'name' => '所属ID',
+            'column' => 'superiorId',
+            'type' => 'String',
+        ),
+        'buyerWebsite' => array(
+            'name' => '买家网站',
+            'column' => 'buyerWebsite',
+            'type' => 'String',
+        ),
+        'buyerWechat' => array(
+            'name' => '买家微信公众号',
+            'column' => 'buyerWechat',
+            'type' => 'String',
+        ),
+        'storeNo' => array(
+            'name' => '店铺号',
+            'column' => 'storeNo',
+            'type' => 'String',
+        ),
+        'storeArea' => array(
+            'name' => '店铺面积',
+            'column' => 'storeArea',
+            'type' => 'String',
+        ),
+        'busiTel' => array(
+            'name' => '营业电话',
+            'column' => 'busiTel',
+            'type' => 'String',
+        ),
+        'employeesNum' => array(
+            'name' => '员工人数',
+            'column' => 'employeesNum',
+            'type' => 'String',
+        ),
+        'paymentType' => array(
+            'name' => '习惯支付方式',
+            'column' => 'paymentType',
+            'type' => 'String',
+        ),
+        'planOrderGap' => array(
+            'name' => '计划订货间隙',
+            'column' => 'planOrderGap',
+            'type' => 'String',
+        ),
+        'planOrderNum' => array(
+            'name' => '计划订货量',
+            'column' => 'planOrderNum',
+            'type' => 'String',
+        ),
+        'actualOrderGap' => array(
+            'name' => '实际订货间隙',
+            'column' => 'actualOrderGap',
+            'type' => 'String',
+        ),
+        'actualOrderNum' => array(
+            'name' => '实际订货量',
+            'column' => 'actualOrderNum',
+            'type' => 'String',
+        ),
+        'marketingsStatus' => array(
+            'name' => '上线状态',
+            'column' => 'marketingsStatus',
+            'type' => 'String',
+        ),
+        'updId' => array(
+            'name' => '更新者ID',
+            'column' => 'updId',
             'type' => 'String',
         ),
     ),
