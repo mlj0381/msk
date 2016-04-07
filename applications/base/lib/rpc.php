c00971065c8b411e19baf1a648647bd23bcadaec
yuanquan24 <yuanquan224@163.com>
2016-04-07 13:38:09 +0800

    基本信息同步

--- applications/base/lib/rpc.php
+++ applications/base/lib/rpc.php
@@ -167,7 +167,7 @@ class base_rpc
 			$this->postData[$column] = $this->_convert($key, $item, $value);		
 		}
 		// 处理参数			
-		if(!$this->status) return ;
+		//if(!$this->status) return ;
 		$this->result = $this->remote();
 	}	
 	private function _convert($key, $item, $data){		
