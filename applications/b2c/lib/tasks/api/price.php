931e1f6118c6b4358088de6b8e9871fb3daf8c20
chen peng <476451758@qq.com>
2016-04-11 11:59:55 +0800

    添加价盘更新定时任务

---
+++ applications/b2c/lib/tasks/api/price.php
@@ -0,0 +1,26 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | VMCSHOP [V M-Commerce Shop]
+// +----------------------------------------------------------------------
+// | Copyright (c) vmcshop.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.vmcshop.com/licensed)
+// +----------------------------------------------------------------------
+// | Author: Shanghai ChenShang Software Technology Co., Ltd.
+// +----------------------------------------------------------------------
+
+
+class b2c_tasks_api_price extends base_task_abstract implements base_interface_task
+{
+    /**
+     * 产品盘价查询 - IPD141111
+     */
+    public function exec($params = null)
+    {
+
+        var_dump('产品盘价查询 - IPD141111');
+        die;
+        return true;
+    }
+}
