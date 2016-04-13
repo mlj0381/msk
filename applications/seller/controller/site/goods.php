e033885447b640b0e7a5aac1921f554b5e3c9e40
hellosnake <1439944212.qq.com>
2016-04-13 14:07:49 +0800

    发布商品

--- applications/seller/controller/site/goods.php
+++ applications/seller/controller/site/goods.php
@@ -409,17 +409,35 @@ class seller_ctl_site_goods extends seller_frontpage
         //基本信息
         $slPdList = Array();
         $catId = explode('-', $goods['api_cat']);
+
+        /*******************************************/
         //获取自己的企业id
-        $brand = app::get('b2c')->model('brand')->getRow('*', array('brand_id' => $goods['brand_id']));
-        //array('uid' => $this->seller['seller_id'], 'from' => '1',
-        $company_id = app::get('base')->model('company')->getRow('ep_id', array('company_id' => $brand['company_id']));
-        //$brand_id = $this->seller['ident'] == 1 ? $company_id['ep_id'] : $company_id['ep_id'];//查询品牌所属企业id
+       // $brand = app::get('b2c')->model('brand')->getRow('*', array('brand_id' => $goods['brand_id']));
+
+       // $company_id = app::get('base')->model('company')->getRow('ep_id', array('company_id' => $brand['company_id']));
+
+        /*******************************************/
+
+
+        $company = app::get('base')->model('company_seller')->getRow('*', array('uid' => $this->seller['seller_id']));
+        $company_id = app::get('base')->model('company')->getRow('*', array('company_id' => $company['company_id']));
+
+
+
+
+
+
+
+
+
+
+
         foreach ($goods['product'] as $v) {
             $slPdList[] = array(
                 'slCode' => $this->seller['sl_code'],
                 'prodEpId' => (int)$company_id['ep_id'],
                 'brandEpId' => (int)$company_id['ep_id'],
-                'brandId' => $brand['api_brand_id'],
+                'brandId' => '111294',//$brand['api_brand_id'],
                 'pdClassesCode' => $catId[0],
                 'machiningCode' => $catId[1],
                 'pdBreedCode' => $catId[2],
