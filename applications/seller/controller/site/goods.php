0a466e6c8b4d2c5cf725fbe5de0898d90d5d432a
hellosnake <1439944212.qq.com>
2016-04-18 17:27:07 +0800

    解决冲突

--- applications/seller/controller/site/goods.php
+++ applications/seller/controller/site/goods.php
@@ -911,7 +911,6 @@ class seller_ctl_site_goods extends seller_frontpage
             $apiData['breedName'] = $_POST['name'];
         }
         $result = $this->app->rpc('apply_for_sku')->request(array('newPdBFWList' => $apiData));
-		print_r($result);
         if($result['status']){
             $this->splash('success', '', '添加成功');
         }
